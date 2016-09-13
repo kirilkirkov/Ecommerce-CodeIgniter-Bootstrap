f2101402adb690c67c950333f2d3f2a84014336d
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-09-13 13:12:07 +0300

    no need this shit

--- application/views/_parts/header.php
+++ application/views/_parts/header.php
@@ -9,7 +9,6 @@
         <meta name="keywords" content="<?= $keywords ?>">
         <meta property="og:image" content="<?= base_url('assets/img/site-overview.png') ?>" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link href="<?= base_url('assets/css/shop-homepage.css') ?>" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.0/css/bootstrap-select.min.css">
         <link href="<?= base_url('assets/css/bootstrap-datepicker.min.css') ?>" rel="stylesheet">

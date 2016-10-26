0f2e478f1c1b5b9a5db0e8c42199d774fae01a56
Kiril Kirkov <kiro@inv.bg>
2016-10-26 11:56:15 +0300

    Bug fix in Textual pages helper

--- application/helpers/gettextualpages_helper.php
+++ application/helpers/gettextualpages_helper.php
@@ -8,5 +8,6 @@ function getTextualPages($activePages)
     $ci = & get_instance();
     $arr = $ci->config->item('no_dynamic_pages');
     $withDuplicates = array_merge($activePages, $arr);
+    if(empty($activePages)) return $activePages;
     return array_diff($withDuplicates, array_diff_assoc($withDuplicates, array_unique($withDuplicates)));
 }

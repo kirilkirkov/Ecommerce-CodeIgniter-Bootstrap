a5230b7b2a5b5d6847b76117d34707f46c7e8706
Kiril Kirkov <kiro@inv.bg>
2016-12-19 11:39:23 +0200

    Bug fix in url routing from mine.js ajax

--- application/views/templates/redlabel/_parts/footer.php
+++ application/views/templates/redlabel/_parts/footer.php
@@ -108,6 +108,12 @@ echo $addedJs;
 <script src="<?= base_url('assets/bootstrap-select-1.12.1/js/bootstrap-select.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/placeholders.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/bootstrap-datepicker.min.js') ?>"></script>
+<script>
+var variable = {
+    clearShoppingCartUrl: "<?= base_url('clearShoppingCart') ?>",
+    manageShoppingCartUrl: "<?= base_url('manageShoppingCart') ?>"
+};
+</script>
 <script src="<?= base_url('templatejs/mine.js') ?>"></script>
 </body>
 </html>

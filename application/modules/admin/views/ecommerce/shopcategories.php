c3b517a0f9d6949c6ad177565c66e6fe7c5f2a4b
Kiril Kirkov <kiro@inv.bg>
2017-02-01 11:27:48 +0200

    Bug fix in words

--- application/modules/admin/views/ecommerce/shopcategories.php
+++ application/modules/admin/views/ecommerce/shopcategories.php
@@ -80,7 +80,7 @@
                 <form action="" method="POST">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="myModalLabel">Add Categorie for magazine</h4>
+                        <h4 class="modal-title" id="myModalLabel">Add Category</h4>
                     </div>
                     <div class="modal-body">
                         <?php foreach ($languages->result() as $language) { ?>

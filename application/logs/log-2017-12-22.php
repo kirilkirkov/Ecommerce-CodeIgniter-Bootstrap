3466ecedd49604433b728ab4d7ec634a75a3fc36
Kiril Kirkov <kiro@inv.bg>
2017-12-22 09:31:25 +0200

    Big fix in admin panel show slider button

---
+++ application/logs/log-2017-12-22.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-12-22 09:29:45 --> 404 Page Not Found: /index
+ERROR - 2017-12-22 09:29:46 --> 404 Page Not Found: /index
+ERROR - 2017-12-22 09:29:46 --> 404 Page Not Found: /index
+ERROR - 2017-12-22 09:30:11 --> 404 Page Not Found: /index

d2eafd310934036c0bf1c21054e12daf4019f978
Kiril Kirkov <kiro@inv.bg>
2018-01-23 10:04:44 +0200

    Remove logs

--- application/logs/log-2018-01-08.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-01-08 08:57:57 --> 404 Page Not Found: /index
--- application/logs/log-2018-01-11.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-01-11 15:29:55 --> 404 Page Not Found: /index
-ERROR - 2018-01-11 15:29:55 --> 404 Page Not Found: /index
-ERROR - 2018-01-11 15:29:55 --> 404 Page Not Found: /index
-ERROR - 2018-01-11 15:29:55 --> 404 Page Not Found: /index
-ERROR - 2018-01-11 17:57:44 --> 404 Page Not Found: /index

046ed2107f45c237af2a49d7f7bbb3752d826a85
Kiril Kirkov <kiro@inv.bg>
2018-02-20 16:53:53 +0200

    when we pass the same currency into the converter we should not try to convert

--- application/helpers/currency_convertor_helper.php
+++ application/helpers/currency_convertor_helper.php
@@ -6,8 +6,11 @@ if (!defined('BASEPATH')) {
 
 function convertCurrency($amount, $from, $to)
 {
-    $data = file_get_contents("https://finance.google.com/finance/converter?a=$amount&from=$from&to=$to"); 
-    preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted); 
+    if ($from == $to) {
+        return number_format(round($amount, 3), 2);
+    }
+    $data = file_get_contents("https://finance.google.com/finance/converter?a=$amount&from=$from&to=$to");
+    preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
     $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
     return number_format(round($converted, 3), 2);
 }

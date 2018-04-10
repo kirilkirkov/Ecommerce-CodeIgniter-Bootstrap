f6461f709a43d92d0c0d2193f10932914fbf85bd
Kiril Kirkov <kiro@inv.bg>
2018-04-10 12:46:07 +0300

    Change dev url. .Dev domains now are auto redirected to https from browser

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://shop.dev/';
+$config['base_url'] = 'http://shop.localhost/';
 
 /*
   |--------------------------------------------------------------------------

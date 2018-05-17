f5753ac381ca075d484ad2a3459cbf3faef9beca
Kiril Kirkov <kiro@inv.bg>
2018-05-17 17:36:44 +0300

    Update elFinder 2.1.9 to 2.1.38

--- application/modules/admin/views/advanced_settings/filemanager.php
+++ application/modules/admin/views/advanced_settings/filemanager.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/js/jquery-ui.css'); ?>">
 <script src="<?= base_url('assets/js/jquery-ui.min.js'); ?>"></script>
-<link rel="stylesheet" type="text/css" href="<?= base_url('assets/elFinder-2.1.9/css/elfinder.min.css'); ?>">
-<link rel="stylesheet" type="text/css" href="<?= base_url('assets/elFinder-2.1.9/css/theme.css'); ?>">
-<script src="<?= base_url('assets/elFinder-2.1.9/js/elfinder.min.js') ?>"></script>
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/elFinder-2.1.38/css/elfinder.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/elFinder-2.1.38/css/theme.css'); ?>">
+<script src="<?= base_url('assets/elFinder-2.1.38/js/elfinder.min.js') ?>"></script>
 <script type="text/javascript" charset="utf-8">
     $(document).ready(function () {
         $('#elfinder').elfinder({
-            url: '<?= base_url('assets/elFinder-2.1.9/php/connector.minimal.php'); ?>'
+            url: '<?= base_url('assets/elFinder-2.1.38/php/connector.minimal.php'); ?>'
         });
     });
 </script>

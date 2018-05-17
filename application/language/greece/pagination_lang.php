c960c75e22a4625dc4d908636730cdf928ce2189
Kiril Kirkov <kiro_tyson@abv.bg>
2018-05-17 11:19:10 +0300

    htm entity fix in greece panination translation

--- application/language/greece/pagination_lang.php
+++ application/language/greece/pagination_lang.php
@@ -1,5 +1,5 @@
 <?php 
-$lang['first'] = ' &pi;&rho;ώ&tau;&omicron;&sigmaf;';
-$lang['last'] = '&tau;&epsilon;&lambda;&epsilon;&upsilon;&tau;&alpha;ί&omicron;&sigmaf;';
-$lang['next'] = '&epsilon;&pi;ό&mu;&epsilon;&nu;&omicron;&sigmaf;';
-$lang['previous'] = '&pi;&rho;&omicron;&eta;&gamma;&omicron;ύ&mu;&epsilon;&nu;&omicron;&sigmaf;';
+$lang['first'] = ' πρώτος';
+$lang['last'] = 'τελευταίος';
+$lang['next'] = 'επόμενος';
+$lang['previous'] = 'προηγούμενος';

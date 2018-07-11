1bdf04ab98c211f276be06b795c6c9878c150b0e
Kiril Kirkov <kiro@inv.bg>
2018-07-11 17:31:15 +0300

    Add email notification for new order

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -9,7 +9,7 @@ class Checkout extends MY_Controller
 
     public function __construct()
     {
-        parent::__construct(); 
+        parent::__construct();
         $this->load->model('admin/Orders_model');
     }
 
@@ -37,6 +37,7 @@ class Checkout extends MY_Controller
                     $this->setVendorOrders();
                     $this->orderId = $orderId;
                     $this->setActivationLink();
+                    $this->sendNotifications();
                     $this->goToDestination();
                 } else {
                     log_message('error', 'Cant save order!! ' . implode('::', $_POST));
@@ -55,6 +56,21 @@ class Checkout extends MY_Controller
     private function setVendorOrders()
     {
         $this->Public_model->setVendorOrder($_POST);
+    }
+
+    /*
+     * Send notifications to users that have nofify=1 in /admin/adminusers
+     */
+
+    private function sendNotifications()
+    {
+        $users = $this->Public_model->getNotifyUsers();
+        $myDomain = $this->config->item('base_url');
+        if (!empty($users)) {
+            foreach ($users as $user) {
+                $this->sendmail->sendTo($user, 'Admin', 'New order in ' . $myDomain, 'Hello, you have new order. Can check it in /admin/orders');
+            }
+        }
     }
 
     private function setActivationLink()

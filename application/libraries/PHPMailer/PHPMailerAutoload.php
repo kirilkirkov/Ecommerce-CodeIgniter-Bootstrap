14193ee12d225720c040ac681e50c97e9b924da8
Kiril Kirkov <kirilkirkov91@gmail.com>
2018-10-28 15:35:48 +0200

    PHPMailer check for deprecated function - _autoload

--- application/libraries/PHPMailer/PHPMailerAutoload.php
+++ application/libraries/PHPMailer/PHPMailerAutoload.php
@@ -42,8 +42,15 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
-    {
-        PHPMailerAutoload($classname);
+    if(function_exists(__autoload)) {
+        function __autoload($classname)
+        {
+            PHPMailerAutoload($classname);
+        }
+    } else {
+        function spl_autoload_register($classname)
+        {
+            PHPMailerAutoload($classname);
+        } 
     }
 }

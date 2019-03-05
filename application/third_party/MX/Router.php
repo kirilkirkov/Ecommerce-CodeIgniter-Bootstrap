71f9aab99b2257994ed0208de6863da1d28151f8
Kiril Kirkov <kiril.kirkov@studiox.bg>
2019-03-05 15:33:05 +0200

    Fix missing suffix

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -236,7 +236,7 @@ class MX_Router extends CI_Router
         public function set_class($class)
         {
             $suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
+		if ($suffix && strpos($class, $suffix) === FALSE)
             {
                 $class .= $suffix;
             }

c25d8883002683e54bdcec1e1a6fa71d035bf352
totoprayogo1916 <mylastof@gmail.com>
2020-08-07 15:48:13 +0700

    Update rest_controller_lang.php

--- application/language/indonesian/rest_controller_lang.php
+++ application/language/indonesian/rest_controller_lang.php
@@ -4,15 +4,15 @@
  * English language
  */
 
-$lang['text_rest_invalid_api_key'] = 'Invalid API key %s'; // %s is the REST API key
-$lang['text_rest_invalid_credentials'] = 'Invalid credentials';
-$lang['text_rest_ip_denied'] = 'IP denied';
-$lang['text_rest_ip_unauthorized'] = 'IP unauthorized';
-$lang['text_rest_unauthorized'] = 'Unauthorized';
-$lang['text_rest_ajax_only'] = 'Only AJAX requests are allowed';
-$lang['text_rest_api_key_unauthorized'] = 'This API key does not have access to the requested controller';
-$lang['text_rest_api_key_permissions'] = 'This API key does not have enough permissions';
-$lang['text_rest_api_key_time_limit'] = 'This API key has reached the time limit for this method';
-$lang['text_rest_ip_address_time_limit'] = 'This IP Address has reached the time limit for this method';
-$lang['text_rest_unknown_method'] = 'Unknown method';
-$lang['text_rest_unsupported'] = 'Unsupported protocol';
+$lang['text_rest_invalid_api_key'] = 'Kunci API %s tidak valid'; // %s is the REST API key
+$lang['text_rest_invalid_credentials'] = 'Kredensial tidak valid';
+$lang['text_rest_ip_denied'] = 'IP ditolak';
+$lang['text_rest_ip_unauthorized'] = 'IP tidak sah';
+$lang['text_rest_unauthorized'] = 'Tidak sah';
+$lang['text_rest_ajax_only'] = 'Hanya permintaan AJAX yang diizinkan';
+$lang['text_rest_api_key_unauthorized'] = 'Kunci API ini tidak memiliki akses ke controller';
+$lang['text_rest_api_key_permissions'] = 'Kunci API ini tidak memiliki izin yang cukup';
+$lang['text_rest_api_key_time_limit'] = 'Kunci IP ini telah mencapai batas waktu untuk metode ini';
+$lang['text_rest_ip_address_time_limit'] = 'Alamat IP ini telah mencapai batas waktu untuk metode ini';
+$lang['text_rest_unknown_method'] = 'Metode tidak diketahui';
+$lang['text_rest_unsupported'] = 'Protokol tidak didukung';
